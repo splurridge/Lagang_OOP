104786b129da6d043e5d463f21cdda27fa620789
Francis James Lagang <144519127+splurridge@users.noreply.github.com>
2023-09-23 20:20:58 +0800

    Add files via upload

---
+++ Employee/src/employee/version3/HourlyEmployee.java
@@ -0,0 +1,60 @@
+package employee.version3;
+
+public class HourlyEmployee extends Employee {
+    private float totalHoursWorked;
+    private float ratePerHour;
+
+    public float getTotalHoursWorked() {
+        return totalHoursWorked;
+    }
+
+    public void setTotalHoursWorked(float totalHoursWorked) {
+        this.totalHoursWorked = totalHoursWorked;
+    }
+
+    public float getRatePerHour() {
+        return ratePerHour;
+    }
+
+    public void setRatePerHour(float ratePerHour) {
+        this.ratePerHour = ratePerHour;
+    }
+
+    public HourlyEmployee(int empID, Name empName, MyDate empDateHired, MyDate empBirthDate, float totalHoursWorked, float ratePerHour) {
+        super(empID, empName, empDateHired, empBirthDate);
+        this.totalHoursWorked = totalHoursWorked;
+        this.ratePerHour = ratePerHour;
+    }
+
+    public HourlyEmployee() {
+    }
+
+    public HourlyEmployee(int empID, Name empName, MyDate empDateHired, MyDate empBirthDate) {
+        super(empID, empName, empDateHired, empBirthDate);
+    }
+
+    public double computeSalary() {
+
+        double totalSalary;
+
+        if(this.totalHoursWorked > 40) {
+            totalSalary = (40 * this.ratePerHour) + ((this.totalHoursWorked - 40)*(this.ratePerHour + (this.ratePerHour * 0.50)));
+        }
+        else {
+            totalSalary = this.totalHoursWorked * this.ratePerHour;
+        }
+
+        return totalSalary;
+    }
+
+    public void displayInfo() {
+        System.out.printf("\nHours worked: %.2f\nRate per hour: ₱ %.02f \nTotal salary: ₱ %.02f\n", totalHoursWorked, ratePerHour, computeSalary());
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() +
+                "\nTotal hours worked: " +this.totalHoursWorked +
+                "\nRate per hour: " + this.ratePerHour;
+    }
+}
---
+++ Employee/src/employee/version3/MyDate.java
@@ -0,0 +1,19 @@
+package employee.version3;
+
+public class MyDate {
+    private int day;
+    private int month;
+    private int year;
+    private String[] monthName = new String[] {"","JAN","FEB","MAR","APR","MAY","JUN","JUL","AUG","SEPT","OCT","NOV","DEC"};
+
+    public MyDate(int day, int month, int year) {
+        this.day = day;
+        this.month = month;
+        this.year = year;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%02d %s %d", this.day, monthName[month], this.year);
+    }
+}
